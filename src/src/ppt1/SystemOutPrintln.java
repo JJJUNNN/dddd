51ee34178e3c7d513855d0d232343fc44563a172
jjj <jjj@naver.com>
2023-09-12 13:59:14 +0900

    first commit

--- src/src/ppt1/SystemOutPrintln.java
+++ src/src/ppt1/SystemOutPrintln.java
@@ -6,7 +6,7 @@ public class SystemOutPrintln {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello,World!! 안녕!!");
+		System.out.println("Hello!!");
 	}
 
 }
